eac2acc7db0dbedbb80fa4e506b505899e332350
venus <autumn.qiu@icloud.come>
2020-10-28 22:26:00 -0400

    Update for server.js

--- public/server.js
+++ public/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const fs = require("fs");
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
